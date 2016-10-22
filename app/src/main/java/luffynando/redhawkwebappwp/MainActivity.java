876f2a36b54e025035b441934f2e01f246df4d63
luffynando <luffynando@gmail.com>
2016-10-22 01:14:46 -0500

    Agregada actividad de ajustes y configuracion de tema en la app.

--- app/src/main/java/luffynando/redhawkwebappwp/MainActivity.java
+++ app/src/main/java/luffynando/redhawkwebappwp/MainActivity.java
@@ -1,5 +1,7 @@
 package luffynando.redhawkwebappwp;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -67,7 +70,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
+            Dialog dialog = new Dialog(MainActivity.this);
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.about_dialog);
+            dialog.show();
             return true;
         }
 
@@ -87,7 +94,8 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
-
+            Intent intent = new Intent(MainActivity.this, Ajustes_Activity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
